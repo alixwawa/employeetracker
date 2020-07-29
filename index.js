28a00e7a92d332e0696bcea4ac12dabef02e417f
Alix Wawa <alixwawa@yahoo.com>
2020-07-29 11:27:49 -0500

    setting up server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const cTable = require('console.table');
+
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "",
+  database: "employeeDB"
+});
